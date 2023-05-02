767498056574962f10489b929244499a59c69a61
saurabhdhebe28 <saurabh.dhebe@vernost.in>
2023-05-02 09:00:09 +0530

    test

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const fileUpload = require("express-fileupload");
 
-const offerRoute = require('./rotues/offersRouter');
-const voucherRoute = require('./rotues/voucherRouter')
+const offerRoute = require("./rotues/offersRouter");
+const voucherRoute = require("./rotues/voucherRouter");
 const app = express();
 
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/offers',offerRoute)
-app.use('/voucher',voucherRoute)
+app.use("/offers", offerRoute);
+app.use("/voucher", voucherRoute);
 
 const port = 3000;
-app.listen(port,()=>{
-    console.log('server is listening on port 3000');
-})
-
+app.listen(port, () => {
+  console.log("server is listening on port 3000");
+});
