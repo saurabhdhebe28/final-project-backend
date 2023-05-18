039448c117db0387d45bc7a9faa94b67a8331b0d
mohif9232 <mohif.waghu@gmail.com>
2023-05-18 14:52:23 +0530

    changes

--- controller/orc.js
+++ controller/orc.js
@@ -1,106 +1,113 @@
-const https = require('https')
-const cheerio = require('cheerio')
-let validatorjs = require('validatorjs')
+const https = require("https");
+const cheerio = require("cheerio");
+let validatorjs = require("validatorjs");
 // const { data } = require('cheerio/lib/api/attributes')
-let userAuthService = new (require('../services/userAuth'))
-let orcRules = new (require('../validation/orc'))
-let orcService = new (require('../services/orc'))
-let orcFormatter = new (require('../formatter/orc'))
+let userAuthService = new (require("../services/userAuth"))();
+let orcRules = new (require("../validation/orc"))();
+let orcService = new (require("../services/orc"))();
+let orcFormatter = new (require("../formatter/orc"))();
 module.exports = class Orc {
-    constructor() { }
-    async addorcFile(request, response) {
-        if (!request.files || !request.body.htmlTemplate) {
-            return response.send({ status: false, data: 'required Contained Not Found' })
-        }
-        let data
-        try {
-            data = orcFormatter.ocrFileData(request.files, request.body)
-            let rules = orcRules.orcFile()
-            let validate = new validatorjs(data, rules)
-            if (validate.fails()) {
-                return response.send(validate.errors)
-            }
-        } 
-        catch (error) {
-            return response.send({ status: false, data: error })
-        }
-
-        let addFile = await orcService.addOcrByfile(data, request.userData).catch((err) => {
-            return { error: err }
-        })
-        if (!addFile || addFile.error) {
-            return response.send(addFile.data)
-        }
-        return response.send(addFile.data)
-
+  constructor() {}
+  async addorcFile(request, response) {
+    if (!request.files || !request.body.htmlTemplate) {
+      return response.send({
+        status: false,
+        data: "required Contained Not Found",
+      });
     }
-    async urlData(request, response) {
-        let rules = await orcRules.url()
-        let validate = await new validatorjs(request.body, rules)
-        if (validate.fails()) {
-            return response.send(validate.errors)
-        }
-        let add = await orcService.addUrl(request.body.url, request.userData).catch((err) => {
-            return { error: err }
-        })
-        if (!add || add.error) {
-            return response.send(add.data)
-        }
-        return response.send(add.data)
-
+    let data;
+    try {
+      data = orcFormatter.ocrFileData(request.files, request.body);
+      let rules = orcRules.orcFile();
+      let validate = new validatorjs(data, rules);
+      if (validate.fails()) {
+        return response.send(validate.errors);
+      }
+    } catch (error) {
+      return response.send({ status: false, data: error });
     }
 
-    async orcList(request, response) {
-        let add = await orcService.orcList(request.userData).catch((err) => {
-            return { error: err }
-        })
-        if (!add || add.error) {
-            return response.send(add.data)
-        }
-        return response.send(add.data)
+    let addFile = await orcService
+      .addOcrByfile(data, request.userData)
+      .catch((err) => {
+        return { error: err };
+      });
+    if (!addFile || addFile.error) {
+      return response.send(addFile.data);
     }
-    async orcListWithSearch(request, response) {
-        let get = await orcService.orcListWithSerach(request.query, request.userData).catch((err) => {
-            return { error: err }
-        })
-        if (!get || get.error) {
-            return response.send(get.data)
-        }
-        return response.send(get.data)
+    return response.send(addFile.data);
+  }
+  async urlData(request, response) {
+    let rules = await orcRules.url();
+    let validate = await new validatorjs(request.body, rules);
+    if (validate.fails()) {
+      return response.send(validate.errors);
+    }
+    let add = await orcService
+      .addUrl(request.body.url, request.userData)
+      .catch((err) => {
+        return { error: err };
+      });
+    if (!add || add.error) {
+      return response.send(add.data);
+    }
+    return response.send(add.data);
+  }
 
+  async orcList(request, response) {
+    try {
+      let add = await orcService.orcList(request.userData).catch((err) => {
+        return { error: err };
+      });
+      if (!add || add.error) {
+        return response.send(add.data);
+      }
+      return response.send(add.data);
+    } catch (error) {
+      return response.send({ status: false, data: error });
+    }
+  }
+  async orcListWithSearch(request, response) {
+    let get = await orcService
+      .orcListWithSerach(request.query, request.userData)
+      .catch((err) => {
+        return { error: err };
+      });
+    if (!get || get.error) {
+      return response.send(get.data);
+    }
+    return response.send(get.data);
+  }
+
+  async downLoadFile(request, response) {
+    let filePath = request.query.filePath;
+    if (!filePath) {
+      return response.send({ status: false, data: "File Path Not Found" });
     }
 
-    async downLoadFile(request, response) {
-        let filePath = request.query.filePath
-        if (!filePath) {
-            return response.send({ status: false, data: "File Path Not Found" })
-        }
+    response.download(filePath, (err) => {
+      if (err) {
+        return response.send({ status: false, data: "Something went wrong" });
+      }
+    });
+  }
 
-        response.download(filePath, (err) => {
-            if (err) {
-                return response.send({ status: false, data: 'Something went wrong' })
-            }
-        })
-
+  async getHeader(request, response) {
+    try {
+      let rules = orcRules.url();
+      let validate = new validatorjs(request.query, rules);
+      if (validate.fails()) {
+        return response.send(validate.errors);
+      }
+    } catch (error) {
+      return response.send({ status: false, data: error });
     }
-
-    async getHeader(request, response) {
-        try {
-            let rules = orcRules.url()
-            let validate = new validatorjs(request.query, rules)
-            if (validate.fails()) {
-                return response.send(validate.errors)
-            }
-        } catch (error) {
-            return response.send({ status: false, data: error })
-        }
-        let getHeader = await orcService.getHeaders(request).catch((err) => {
-            return { error: err }
-        })
-        if (!getHeader || getHeader.error) {
-            return response.send(getHeader.data)
-        }
-        return response.send(getHeader.data)
+    let getHeader = await orcService.getHeaders(request).catch((err) => {
+      return { error: err };
+    });
+    if (!getHeader || getHeader.error) {
+      return response.send(getHeader.data);
     }
-}
-
+    return response.send(getHeader.data);
+  }
+};
--- services/orc.js
+++ services/orc.js
@@ -1,243 +1,243 @@
-const cheerio = require('cheerio')
-let orcRules = new (require('../validation/orc'))
-const validatorjs = require('validatorjs')
-let orcModel = new (require('../model/orc'))
-let moment = require('moment')
-const config = require('../config')
-const axios = require('axios')
-
+const cheerio = require("cheerio");
+let orcRules = new (require("../validation/orc"))();
+const validatorjs = require("validatorjs");
+let orcModel = new (require("../model/orc"))();
+let moment = require("moment");
+const config = require("../config");
+const axios = require("axios");
 
 module.exports = class Orc {
-    constructor() { }
-    async getHeaders(param) {
-        let response
-        try {
-            response = await axios.get(param.query.url)
-
-        } catch (error) {
-            return {
-                data: {
-                    status: false,
-                    data: error
-                }
-            }
-        }
-        return {
-            data: {
-                status: true,
-                data: response.headers
-            }
-        }
-
+  constructor() {}
+  async getHeaders(param) {
+    let response;
+    try {
+      response = await axios.get(param.query.url);
+    } catch (error) {
+      return {
+        data: {
+          status: false,
+          data: error,
+        },
+      };
     }
-    async addOcrByfile(param, userData) {
-        if (!param.file || !param.htmlTemplate || (typeof param.file != 'object')) {
-            return {
-                data: {
-                    status: false,
-                    data: 'Required Data Not Found Please try with Accurate File'
-                }
-            }
-        }
-        let fileInfo = param.file
-        let fileName = fileInfo.name
-        let htmlContent = param.htmlTemplate
-        let uploadPath = config.ocrUploadPath + fileName
-        let a = uploadPath.split('/')
-        let b = a.reverse()
-        let dbPath = '/' + b[2] + '/' + b[1] + '/' + b[0]
-
-        let $ = cheerio.load(htmlContent)
-        let urlData = {
-            requestedBy: $('#requestedByLabel').text().trim(),
-            signedBy: $('#signedByLabel').text().trim(),
-            totalCounter: $('#totalCounterLabel').text().trim(),
-            // sdcTime: $('#sdcDateTimeLabel').text().trim(),
-            // sdcTime: await STR_TO_DATE($('#sdcDateTimeLabel').text().trim(), '%d.%m.%Y. %H:%i:%s'),
-            sdcTime: await moment($('#sdcDateTimeLabel').text().trim(), 'DD.MM.YYYY. HH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
-            tin: $('#tinLabel').text().trim(),
-            locationName: $('#shopFullNameLabel').text().trim(),
-            totalAmount: $('#totalAmountLabel').text().trim(),
-            address: $('#addressLabel').text().trim(),
-            city: $('#cityLabel').text().trim(),
-            transactionTypeCounter: $('#transactionTypeCounterLabel').text().trim(),
-            location: dbPath,
-            user_id: userData.id
-        }
-
-        try {
-            let rules = orcRules.orcDataObject()
-            let validate = new validatorjs(urlData, rules)
-            if (validate.fails()) {
-                return {
-                    data: {
-                        status: false,
-                        data: 'This input doesnt contain the expected value please enter proper url'
-                    }
-                }
-            }
-        } catch (error) {
-            return {
-                data: {
-                    status: false,
-                    data: error
-                }
-            }
-        }
-
-        let addData = await orcModel.addUrlData(urlData).catch((err) => {
-            return { error: err }
-        })
-        if (!addData || addData.error) {
-            return {
-                data: {
-                    status: false,
-                    data: addData.error.sqlMessage
-                }
-            }
-        }
-        try {
-            fileInfo.mv(uploadPath)
-        } catch (error) {
-            return {
-                data: {
-                    status: false,
-                    data: error
-                }
-            }
-        }
-        return {
-            data: {
-                status: true,
-                data: 'Data inserted'
-            }
-        }
-
+    return {
+      data: {
+        status: true,
+        data: response.headers,
+      },
+    };
+  }
+  async addOcrByfile(param, userData) {
+    if (!param.file || !param.htmlTemplate || typeof param.file != "object") {
+      return {
+        data: {
+          status: false,
+          data: "Required Data Not Found Please try with Accurate File",
+        },
+      };
     }
-    async addUrl(url, userData) {
-        let htmlSyntax
-        let value
-        try {
-            htmlSyntax = await axios.get(url)
-            value = await htmlSyntax.data
-        } catch (error) {
-            return {
-                data: {
-                    status: false,
-                    data: error.message
-                }
-            }
-        }
+    let fileInfo = param.file;
+    let fileName = fileInfo.name;
+    let htmlContent = param.htmlTemplate;
+    let uploadPath = config.ocrUploadPath + fileName;
+    let a = uploadPath.split("/");
+    let b = a.reverse();
+    let dbPath = "/" + b[2] + "/" + b[1] + "/" + b[0];
 
-        let $ = cheerio.load(value)
-        let urlData = {
-            requestedBy: $('#requestedByLabel').text().trim(),
-            signedBy: $('#signedByLabel').text().trim(),
-            totalCounter: $('#totalCounterLabel').text().trim(),
-            // sdcTime: $('#sdcDateTimeLabel').text().trim(),
-            // sdcTime: await STR_TO_DATE($('#sdcDateTimeLabel').text().trim(), '%d.%m.%Y. %H:%i:%s'),
-            sdcTime: await moment($('#sdcDateTimeLabel').text().trim(), 'DD.MM.YYYY. HH:mm:ss').format('YYYY-MM-DD HH:mm:ss'),
-            tin: $('#tinLabel').text().trim(),
-            locationName: $('#shopFullNameLabel').text().trim(),
-            totalAmount: $('#totalAmountLabel').text().trim(),
-            address: $('#addressLabel').text().trim(),
-            city: $('#cityLabel').text().trim(),
-            transactionTypeCounter: $('#transactionTypeCounterLabel').text().trim(),
-            location: url,
-            user_id: userData.id
-        }
+    let $ = cheerio.load(htmlContent);
+    let urlData = {
+      requestedBy: $("#requestedByLabel").text().trim(),
+      signedBy: $("#signedByLabel").text().trim(),
+      totalCounter: $("#totalCounterLabel").text().trim(),
+      // sdcTime: $('#sdcDateTimeLabel').text().trim(),
+      // sdcTime: await STR_TO_DATE($('#sdcDateTimeLabel').text().trim(), '%d.%m.%Y. %H:%i:%s'),
+      sdcTime: await moment(
+        $("#sdcDateTimeLabel").text().trim(),
+        "DD.MM.YYYY. HH:mm:ss"
+      ).format("YYYY-MM-DD HH:mm:ss"),
+      tin: $("#tinLabel").text().trim(),
+      locationName: $("#shopFullNameLabel").text().trim(),
+      totalAmount: $("#totalAmountLabel").text().trim(),
+      address: $("#addressLabel").text().trim(),
+      city: $("#cityLabel").text().trim(),
+      transactionTypeCounter: $("#transactionTypeCounterLabel").text().trim(),
+      location: dbPath,
+      user_id: userData.id,
+    };
 
-        let rules = await orcRules.orcDataObject()
-        let validate = await new validatorjs(urlData, rules)
-        if (validate.fails()) {
-            return {
-                data: {
-                    status: false,
-                    data: 'This url doesnt contain the expected value please enter proper url'
-                }
-            }
-        }
-        let addData = await orcModel.addUrlData(urlData).catch((err) => {
-            return { error: err }
-        })
-        if (!addData || addData.error) {
-            return {
-                data: {
-                    status: false,
-                    data: addData.error.sqlMessage
-                }
-            }
-        }
+    try {
+      let rules = orcRules.orcDataObject();
+      let validate = new validatorjs(urlData, rules);
+      if (validate.fails()) {
         return {
-            data: {
-                status: true,
-                data: 'Data inserted'
-            }
-        }
+          data: {
+            status: false,
+            data: "This input doesnt contain the expected value please enter proper url",
+          },
+        };
+      }
+    } catch (error) {
+      return {
+        data: {
+          status: false,
+          data: error,
+        },
+      };
     }
 
-    async orcList(userData) {
-        let folderPath = config.ocrUploadPath
-        let data
-        try {
-            data = await orcModel.orcDataList(userData.id)
-        } catch (error) {
-            return {
-                data: {
-                    status: false,
-                    data: error.message
-                }
-            }
-        }
-        if (data.length == 0) {
-            return {
-                data: {
-                    status: false,
-                    data: 'Not Found'
-                }
-            }
-        }
-        for (let a in data) {
-            data[a].isUrl = true
-            let check = data[a].location.split('/')
-            if (check[0] == '') {
-                data[a].location = folderPath + data[a].location
-                data[a].isUrl = false
-            }
-        }
-        return {
-            data: {
-                status: true,
-                data: data
-            }
-        }
-
+    let addData = await orcModel.addUrlData(urlData).catch((err) => {
+      return { error: err };
+    });
+    if (!addData || addData.error) {
+      return {
+        data: {
+          status: false,
+          data: addData.error.sqlMessage,
+        },
+      };
+    }
+    try {
+      fileInfo.mv(uploadPath);
+    } catch (error) {
+      return {
+        data: {
+          status: false,
+          data: error,
+        },
+      };
+    }
+    return {
+      data: {
+        status: true,
+        data: "Data inserted",
+      },
+    };
+  }
+  async addUrl(url, userData) {
+    let htmlSyntax;
+    let value;
+    try {
+      htmlSyntax = await axios.get(url);
+      value = await htmlSyntax.data;
+    } catch (error) {
+      return {
+        data: {
+          status: false,
+          data: error.message,
+        },
+      };
     }
 
+    let $ = cheerio.load(value);
+    let urlData = {
+      requestedBy: $("#requestedByLabel").text().trim(),
+      signedBy: $("#signedByLabel").text().trim(),
+      totalCounter: $("#totalCounterLabel").text().trim(),
+      // sdcTime: $('#sdcDateTimeLabel').text().trim(),
+      // sdcTime: await STR_TO_DATE($('#sdcDateTimeLabel').text().trim(), '%d.%m.%Y. %H:%i:%s'),
+      sdcTime: await moment(
+        $("#sdcDateTimeLabel").text().trim(),
+        "DD.MM.YYYY. HH:mm:ss"
+      ).format("YYYY-MM-DD HH:mm:ss"),
+      tin: $("#tinLabel").text().trim(),
+      locationName: $("#shopFullNameLabel").text().trim(),
+      totalAmount: $("#totalAmountLabel").text().trim(),
+      address: $("#addressLabel").text().trim(),
+      city: $("#cityLabel").text().trim(),
+      transactionTypeCounter: $("#transactionTypeCounterLabel").text().trim(),
+      location: url,
+      user_id: userData.id,
+    };
 
-    async orcListWithSerach(param, userData) {
-console.log(param);
-        let requestedBy = param.requestedBy ? param.requestedBy : ''
-        let tin = param.tin ? param.tin : ''
-        let city = param.city ? param.city : ''
-
-        let search = await orcModel.OcrListBysearch(requestedBy, tin, city, userData.id).catch((err) => {
-            return { error: err }
-        })
-        if (!search || search.error) {
-            return {
-                data: {
-                    status: false,
-                    data: search.error.sqlMessage
-                }
-            }
-        }
-        return {
-            data: {
-                status: true,
-                data: search
-            }
-        }
+    let rules = await orcRules.orcDataObject();
+    let validate = await new validatorjs(urlData, rules);
+    if (validate.fails()) {
+      return {
+        data: {
+          status: false,
+          data: "This url doesnt contain the expected value please enter proper url",
+        },
+      };
     }
+    let addData = await orcModel.addUrlData(urlData).catch((err) => {
+      return { error: err };
+    });
+    if (!addData || addData.error) {
+      return {
+        data: {
+          status: false,
+          data: addData.error.sqlMessage,
+        },
+      };
+    }
+    return {
+      data: {
+        status: true,
+        data: "Data inserted",
+      },
+    };
+  }
 
-}
+  async orcList(userData) {
+    let folderPath = config.ocrUploadPath;
+    let data;
+    try {
+      data = await orcModel.orcDataList(userData.id);
+    } catch (error) {
+      return {
+        data: {
+          status: false,
+          data: error.message,
+        },
+      };
+    }
+    if (data.length == 0) {
+      return {
+        data: {
+          status: false,
+          data: "Not Found",
+        },
+      };
+    }
+    for (let a in data) {
+      data[a].isUrl = true;
+      let check = data[a].location.split("/");
+      if (check[0] == "") {
+        data[a].location = folderPath + data[a].location;
+        data[a].isUrl = false;
+      }
+    }
+    return {
+      data: {
+        status: true,
+        data: data,
+      },
+    };
+  }
+
+  async orcListWithSerach(param, userData) {
+    let requestedBy = param.requestedBy ? param.requestedBy : "";
+    let tin = param.tin ? param.tin : "";
+    let city = param.city ? param.city : "";
+
+    let search = await orcModel
+      .OcrListBysearch(requestedBy, tin, city, userData.id)
+      .catch((err) => {
+        return { error: err };
+      });
+    if (!search || search.error) {
+      return {
+        data: {
+          status: false,
+          data: search.error.sqlMessage,
+        },
+      };
+    }
+    return {
+      data: {
+        status: true,
+        data: search,
+      },
+    };
+  }
+};
